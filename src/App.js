4f70245f89b44d1ece459cf074973c0374476873
Hiba <hibat64@gmail.com>
2020-08-02 22:23:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MovieList from './components/MovieList'
 import './App.css';
 
+import AddMovie from './components/AddMovie';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [Movie,setMovie]=useState([{titre:"Annihilation",rate:4,image:"https://images.squarespace-cdn.com/content/v1/5ae7f97a9772ae4ce53566b2/1554356845910-EI2ZVHJRKOSPJAHCUAI1/ke17ZwdGBToddI8pDm48kMXRibDYMhUiookWqwUxEZ97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0luUmcNM2NMBIHLdYyXL-Jww_XBra4mrrAHD6FMA3bNKOBm5vyMDUBjVQdcIrt03OQ/ANNIHILATION+poster_smaller.jpg"},{titre:"Starwars",rate:3,image:"https://starwarsblog.starwars.com/wp-content/uploads/2015/04/star-wars-the-digital-movie-collection.jpg"},{titre:"Avengers",rate:5,image:"https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg"}])
+  const AjoutMovie=(a,b,c)=>{
+    setMovie([...Movie,{titre:a,rate:b,image:c}])
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <MovieList movie={Movie}/>
+     <AddMovie AjoutMovie={AjoutMovie}/>
     </div>
   );
 }
 
 export default App;
+
